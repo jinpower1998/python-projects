eeff83d8830d7e31e24f067540835c774447af4e
Me <jinpower98@googlemail.com>
2022-09-27 18:45:01 +0200

    remove db-option + add post-request

--- flask/myapp2.0/app.py
+++ flask/myapp2.0/app.py
@@ -1,27 +1,16 @@
+import email
+from time import sleep
+import requests, urllib
 from flask import Flask, render_template, url_for, request
-from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import SQLAlchemy
 
 
 #create flask-app
 app = Flask(__name__)
 
-# create  db-extension
-db = SQLAlchemy()
-# configure the SQLite database, relative to the app instance folder
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///user_data.db"
-# initialize the app with the extension
-db.init_app(app)
+# post request
+rq = requests.post
 
-#class for storing creds in db
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String, unique=True, nullable=False)
-    firstname = db.Column(db.String)
-    lastname = db.Column(db.String)
-    password = db.Column(db.String)
-
-with app.app_context():
-    db.create_all()
 
 #class for Credentials
 class Creds():
@@ -31,27 +20,17 @@ class Creds():
         self.email = email
         self.passwd = passwd
         
-# create user in db
-def create_db_user_data(firstname, lastname, email, password):
-    user = User(
-            __fname__ = firstname,
-            __lname__ = lastname,
-            __email__ = email,
-            __passwd__ = password
-        )   
-                    
-    db.session.add(user)
-    db.session.commit()        
-    return user
+      
+#     return user
 
 #main site
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
 def welcome_site():
     print(request)
     return render_template("main.html")
 
 #registration site
-@app.route("/register", methods=['GET', 'POST'])
+@app.route("/register", methods=['GET','POST'])
 def register():
 
     args = request.args
@@ -59,22 +38,34 @@ def register():
     Creds.lname = args.get("lname")
     Creds.email = args.get("email")
     Creds.passwd = args.get("passwd")
-        
+
+    user_data = {
+        "Firstname" : Creds.fname,
+        "Lastname" : Creds.lname,
+        "Email" : Creds.email,
+        "Password" : Creds.passwd
+    }
+
     print(request)
 
-    if request.method=="GET":
-        if "Submit" in request.form:
+#post data to external database
+    if request.method=="POST":
+        if request.form.get("submit_button") == "Submit":
+            print(user_data)
+        else:
             print("REQUEST SUBMITTED !")
+            pass
     
-    return render_template("Register.html", fname=Creds.fname, lname=Creds.lname, email=Creds.email, passwd=Creds.passwd)
+    return render_template("Register.html",  fname=Creds.fname, lname=Creds.lname, email=Creds.email, passwd=Creds.passwd)
 
-# @app.route("/users")
-# def user_list():
-#     users = db.session.execute(db.select(User).order_by(User.email)).all()
-#     return render_template("user/list.html", users=users)
+
+#enter site
+@app.route("/enter", methods=['GET','POST'])
+def complete_registration():
+    return render_template("enter.html")
 
 #login site
-@app.route("/login")
+@app.route("/login",methods=['GET','POST'])
 def login():
     args = request.args
     Creds.email = args.get("email")
@@ -84,11 +75,13 @@ def login():
     return render_template("Login.html", email=Creds.email, passwd=Creds.passwd)
 
 
+
+
 if __name__ == "__main__":
 #    app.run()
     app.run(host="0.0.0.0", port="80")
 
 
-app.run(debug=True)
+#app.run(debug=True)
 
 
